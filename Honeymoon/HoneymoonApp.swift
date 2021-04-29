0384aa11ab7dad3481d2d084c3c52cf5f1d4865a
Ian Vilhena <ivilhena.solks@digio.com.br>
2021-04-29 14:05:59 -0300

    Initial Commit

---
+++ Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Ian Vilhena on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
